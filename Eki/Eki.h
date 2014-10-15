a97b39e0a87cfbcd4e403e0c4e897beaf3123c1d
Jeremy Marchand <kodlian@users.noreply.github.com>
2014-10-15 15:50:46 +0200

    Initial Commit

---
+++ Eki/Eki.h
@@ -0,0 +1,19 @@
+//
+//  Eki.h
+//  Eki
+//
+//  Created by Jérémy Marchand on 15/10/14.
+//  Copyright (c) 2014 Jérémy Marchand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Eki.
+FOUNDATION_EXPORT double EkiVersionNumber;
+
+//! Project version string for Eki.
+FOUNDATION_EXPORT const unsigned char EkiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Eki/PublicHeader.h>
+
+
