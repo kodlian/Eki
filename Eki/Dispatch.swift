af2155c8849d7bf962c317584dabc3ac66ee9ca5
Jeremy Marchand <kodlian@users.noreply.github.com>
2014-10-15 18:18:52 +0200

    refactoring - use enum instead of class

--- Eki/Dispatch.swift
+++ Eki/Dispatch.swift
@@ -8,60 +8,81 @@
 
 import Foundation
 
-/// Grand Central Dispatch Queue wrapper
-public class DispatchQueue {
-    /// Dispatch Priority
-    public enum Priority {
-        case Background, Default, High, Low
-        
-        func queue() -> dispatch_queue_t {
-            
-            var priority:Int = 0
-            
-            switch (self) {
-            case .Background:
-                priority = DISPATCH_QUEUE_PRIORITY_BACKGROUND
-            case .Default:
-                priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-            case .High:
-                priority = DISPATCH_QUEUE_PRIORITY_HIGH
-            case .Low:
-                priority = DISPATCH_QUEUE_PRIORITY_LOW
-                
-            }
-            return dispatch_get_global_queue(priority, 0)
+/**
+A wrapper for Grand Central Dispatch Queue
+*/
+public enum DispatchQueue {
+    /**
+    The serial queue associated with the application’s main thread
+    */
+    case Main
+    
+    /**
+    A system-defined global concurrent queue with a User Interactive quality of service class.
+    */
+    case UserInteractive
+    
+    /**
+    A system-defined global concurrent queue with a User Initiated quality of service class.
+    */
+    case UserInitiated
+    
+    /**
+    A system-defined global concurrent queue with a Default quality of service class.
+    */
+    case Default
+    
+    /**
+    A system-defined global concurrent queue with a Utility quality of service class.
+    */
+    case Utility
+    
+    /**
+    A system-defined global concurrent queue with a Background quality of service class.
+    */
+    case Background
+
+    case Concurrent(name:String)
+    case Serial(name:String)
+    case Custom(queue:dispatch_queue_t)
+
+    
+    /**
+   
+    */
+    func queue() -> dispatch_queue_t {
+        switch (self) {
+        case .Main:
+            return dispatch_get_main_queue()
+        case .UserInteractive:
+            return dispatch_get_global_queue(Int(QOS_CLASS_USER_INTERACTIVE.value), 0)
+        case .UserInitiated:
+            return dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.value), 0)
+        case .Default:
+            return dispatch_get_global_queue(Int(QOS_CLASS_DEFAULT.value), 0)
+        case .Utility:
+            return dispatch_get_global_queue(Int(QOS_CLASS_UTILITY.value), 0)
+        case .Background:
+            return dispatch_get_global_queue(Int(QOS_CLASS_BACKGROUND.value), 0)
+        case .Concurrent(let name):
+            return dispatch_queue_create(name,DISPATCH_QUEUE_CONCURRENT)
+        case .Serial(let name):
+            return dispatch_queue_create(name,DISPATCH_QUEUE_SERIAL)
+        case .Custom(let queue):
+            return queue
         }
         
-    }
-    public typealias Delay = dispatch_time_t
-
-    private(set) var queue:dispatch_queue_t
-    
-    //MARK: Convenient queue
-    public class var mainQueue:DispatchQueue {
-        return DispatchQueue(queue: dispatch_get_main_queue())
-    }
-
-    //MARK: Initializers
-    public init( queue q:dispatch_queue_t) {
-        queue = q
-    }
-    public init( priority:Priority) {
-        queue = priority.queue()
-    }
-    public init( backgroundQueueName:String) {
-        queue = dispatch_queue_create(backgroundQueueName, nil)
     }
 
     //MARK: Dispatch function
     public func asyncBlock(block:() -> ()) {
-        dispatch_async(queue, block)
+        dispatch_async(queue(), block)
     }
     public func syncBlock(block:() -> ()) {
-        dispatch_sync(queue, block)
+        dispatch_sync(queue(), block)
     }
-    public func after(delay:Delay,  block:() -> ()) {
-        dispatch_after(delay, queue, block)
+    public func after(delay:NSTimeInterval,  block:() -> ()) {
+        dispatch_after(dispatch_time_t(delay) * dispatch_time_t(NSEC_PER_SEC), queue(), block)
     }
 }
 
@@ -71,13 +92,13 @@ public func once(block:() -> ()) {
     dispatch_once(&token,block)
 }
 public func async(block:() -> ()) {
-    DispatchQueue.mainQueue.asyncBlock(block)
+    DispatchQueue.Main.asyncBlock(block)
 }
 public func sync(block:() -> ()) {
-    DispatchQueue.mainQueue.syncBlock(block)
+    DispatchQueue.Main.syncBlock(block)
 }
-public func after(delay:DispatchQueue.Delay,  block:() -> ()) {
-    DispatchQueue.mainQueue.after(delay, block:block)
+public func after(delay:NSTimeInterval,  block:() -> ()) {
+    DispatchQueue.Main.after(delay, block:block)
 }
 
 
