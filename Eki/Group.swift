6d704fb63aafbdf66faaf0e15ab8223e8f25cf45
Jeremy Marchand <kodlian@users.noreply.github.com>
2015-03-04 15:09:39 +0100

    Update for swift 1.2 + refactoring

--- Eki/Group.swift
+++ Eki/Group.swift
@@ -12,7 +12,7 @@ import Foundation
 @abstract
 A wrapper for Grand Central Dispatch Group
 */
-public struct Group{
+public struct Group {
     private var group = dispatch_group_create();
     public var defaultQueue:Queue = Queue.Background {
         didSet {
@@ -22,23 +22,31 @@ public struct Group{
     private var defaultDispatchQueue = Queue.Background.dispatchQueue()
     
     
-    init(defaultQueue:Queue ) {
+    init(defaultQueue:Queue = Queue.Background) {
         self.defaultQueue = defaultQueue
     }
     
 
-    //MARK: Dispatch blocks
-    public func dispatch(block:() -> Void, onQueue queue:Queue? = nil)  -> Group {
-        dispatch_group_async(group,queue?.dispatchQueue() ?? defaultDispatchQueue, block)
+    //MARK: Dispatch
+    public func dispatch(block:() -> Void)  -> Group {
+        dispatchOnQueue(nil,block:block)
+        return self
+    }
+    public func dispatchOnQueue(queue:Queue?,block:() -> Void )  -> Group {
+        dispatch_group_async(group,  queue?.dispatchQueue() ?? defaultDispatchQueue, block)
         return self
     }
     public func dispatchOperation(operation:Operation)  -> Group {
-        dispatch(operation.block, onQueue:operation.queue)
+        dispatchOnQueue(operation.queue, block:operation.block)
         return self
     }
-    public func dispatch(blocks:[() -> Void], onQueue queue:Queue? = nil) -> Group {
+    public func dispatch(blocks:[() -> Void]) -> Group {
+        dispatchOnQueue(nil,blocks:blocks)
+        return self
+    }
+    public func dispatchOnQueue(queue:Queue?, blocks:[() -> Void]) -> Group {
         for block in blocks {
-            dispatch(block,onQueue:queue)
+            dispatchOnQueue(queue,block:block)
         }
         return self
     }
@@ -50,20 +58,15 @@ public struct Group{
     }
     
     //MARK: Others
-    public func notifyCompletionOnBlock(block:() -> Void, queue:Queue? = nil) -> Group {
-        dispatch_group_notify(group,queue?.dispatchQueue() ?? defaultDispatchQueue, block);
-        
+    public func notifyCompletion(block:() -> Void) -> Group {
+        return notifyCompletionOnQueue(defaultQueue,block: block)
+    }
+    public func notifyCompletionOnQueue(queue:Queue, block:() -> Void) -> Group {
+        dispatch_group_notify(group,queue.dispatchQueue(), block);
         return self
     }
-    
-    public func wait() {
+    public func wait() -> Group {
         dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+        return self
     }
-}
-func test() {
-
-//    let g = Group(defaultQueue:Queue.Main).dispatch{ () -> Void in
-//        
-//    }
-    
 }
